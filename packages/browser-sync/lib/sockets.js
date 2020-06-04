5c2e0b4523a2055ea38dfdb4a3344745f75386a5
wugaoping <gaoping@kezhiyin.com>
2020-06-04 14:44:22 +0800

    支持通道同步功能

--- packages/browser-sync/lib/sockets.js
+++ packages/browser-sync/lib/sockets.js
@@ -12,6 +12,17 @@ module.exports.plugin = function(server, clientEvents, bs) {
 };
 
 /**
+ * socket ----> channel_id
+ * 记录连接归属的通道
+ */
+var socketChannelMap = {}
+/**
+ * channel ----> socket list
+ * 记录改连接所有的通道列表
+ */
+var channelSocketsMap = {}
+
+/**
  * @param {http.Server} server
  * @param clientEvents
  * @param {BrowserSync} bs
@@ -62,6 +73,36 @@ module.exports.init = function(server, clientEvents, bs) {
         emitter.emit("client:connected", {
             ua: client.handshake.headers["user-agent"]
         });
+
+        // 如果启用了通道功能，则需要维护通道中的连接信息
+        if (bs.options.get("channel") === true) {
+            var cookies = client.handshake.headers["cookie"]
+            var cidReg = /x-bs-channel-id=[^;\s]+/g
+            var cidArr = cookies.match(cidReg)
+            var xcid = cidArr && cidArr[0]
+            // 如果该连接存在通道标识，则将该连接加入到通道
+            if (xcid) {
+                var index = 'x-bs-channel-id='.length
+                xcid = xcid.substring(index)
+                socketChannelMap[client] = xcid
+                var sockets = channelSocketsMap[xcid] || []
+                sockets.push(client)
+                channelSocketsMap[xcid] = sockets
+            }
+
+            client.on('disconnect', function() {
+                // 连接断开后需要清除连接与通道的映射关系
+                var channel = socketChannelMap[client]
+                if (channel) {
+                    delete socketChannelMap[client]
+                    var sockets = channelSocketsMap[channel]
+                    var index = sockets.indexOf(client)
+                    if (index >= 0) {
+                        channelSocketsMap[channel] = sockets.splice(index, 1)
+                    }
+                }
+            })
+        }
     }
 
     /**
@@ -72,7 +113,35 @@ module.exports.init = function(server, clientEvents, bs) {
 
         function addEvent(event) {
             client.on(event, (data) => {
-                client.broadcast.emit(event, data);
+                // 如果启用了通道功能，则消息仅发给相同通道中的连接
+                if (bs.options.get("channel") === true) {
+                    var channel = socketChannelMap[client]
+                    // 如果该连接归属某个通道，单独发给该通道中的所有其他连接
+                    if (channel) {
+                        var sockets = channelSocketsMap[channel]
+                        for (var i=0; i<sockets.length; i++) {
+                            var socket = sockets[i]
+                            if (socket !== client) {
+                                try {
+                                    socket.emit(event, data)
+                                } catch (e) {
+                                    // 将异常连接进行移除
+                                    delete socketChannelMap[socket]
+                                    var index = sockets.indexOf(socket)
+                                    if (index >= 0) {
+                                        channelSocketsMap[channel] = sockets.splice(index, 1)
+                                    }
+                                    // 移除一个socket数组元素后，数组元素整体前移
+                                    i--
+                                }
+                            }
+                        }
+                    }
+                }
+                // 如果没有启动通道功能，则广播给所有的连接
+                else {
+                    client.broadcast.emit(event, data);
+                }
             });
         }
     }
